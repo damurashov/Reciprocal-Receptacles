a0d94e3c94e3769086457a2a67aff200a69d7f4a
DmitryMurashov <dmtr.murashov@gmail.com>
2021-11-29 13:53:47 +0300

    [options.cmake] Bug Build | CMake / C define translation

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,10 +1,15 @@
 
 add_library(Options INTERFACE)
 
-macro(rr_option option_name option_description option_default)
+macro(rr_bin_option option_name option_description option_default)
 	option(${option_name} ${option_description} ${option_default})
-	target_compile_definitions(Options INTERFACE ${option_name}=${${option_name}})
+
+	if(${option_name})
+		target_compile_definitions(Options INTERFACE -D${option_name}=1)
+	else()
+		target_compile_definitions(Options INTERFACE -D${option_name}=0)
+	endif()
 endmacro()
 
 # C++ standard library
-rr_option(RRO_STL_ENABLE "Enable C++ STL standard library" ON)
+rr_bin_option(RRO_STL_USED "C++ STL standard library is being used" ON)
