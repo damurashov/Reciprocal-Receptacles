6f63ec3edc2b9c50bec2731af7cfe4aa7a90a960
DmitryMurashov <dmtr.murashov@gmail.com>
2021-12-07 16:57:26 +0300

    [util: DefaultConfig] Enh | C++ checking version

--- src/Rr/Util/DefaultConfig.hpp
+++ src/Rr/Util/DefaultConfig.hpp
@@ -1,15 +1,19 @@
 //
 // DefaultConfig.hpp
 //
-// Author: Dmitry Murashov 
+// Author: Dmitry Murashov
 //   mail: dmtr D murashov A gmail D com (Obfuscated to protect from spambots. Replace "D" with ".", "A" with "@")
 //
 // Provides default configuration for the library for cases when for some reason
 // it should be configured w\o the use of CMake, i.e. manually.
 //
 
+#if __cplusplus < 201103L
+# error " RR requires at least C++11"
+#endif
 
 // Use C++ STL
 #if !defined(RRO_STL_USED)
 #define RRO_STL_USED 1
+#include <type_traits>  // Test
 #endif // RRO_STL_USED
