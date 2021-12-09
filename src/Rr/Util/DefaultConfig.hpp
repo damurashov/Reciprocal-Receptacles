c49b62d8aa4d070f9ac029a0b3d1a834006c01c4
DmitryMurashov <dmtr.murashov@gmail.com>
2021-12-09 16:47:27 +0300

    [util: DefaultConfig] Impl | RRO_SAFE_FN_CONVERSION flag

--- src/Rr/Util/DefaultConfig.hpp
+++ src/Rr/Util/DefaultConfig.hpp
@@ -17,3 +17,13 @@
 #define RRO_STL_USED 1
 #include <type_traits>  // Test
 #endif // RRO_STL_USED
+
+// Use static_cast for callable member casting.
+// For example, modern gcc allows one to convert, say, void(D::*)(int) to
+// void(Z::*)() and USE the latter although the types are not related in any
+// way. With this flag in place, one would be able to use the library in the
+// same way, but with deriving the callable objects from Rr::RrObject
+// https://stackoverflow.com/questions/37662100/calling-derived-class-through-base-class-function-pointer
+#if !defined(RRO_SAFE_FN_CONVERSION)
+# define RRO_SAFE_FN_CONVERSION 0
+#endif
