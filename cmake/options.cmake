f649f58c3a959423cf82f0e87787283856fe5069
DmitryMurashov <dmtr.murashov@gmail.com>
2021-12-11 18:52:19 +0300

    [options.cmake] Build | Bug Cascade | STATIC_CAST_FN_CONVERSION option - renamed

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -15,4 +15,4 @@ endmacro()
 rr_bin_option(RRO_STL_USED "C++ STL standard library is being used" ON)
 
 # Safe callback conversion
-rr_bin_option(RRO_SAFE_FN_CONVERSION "Use static_cast to convert callbacks. Requires deriving from base type" OFF)
+rr_bin_option(RRO_STATIC_CAST_FN_CONVERSION "Use static_cast to convert callbacks. Requires deriving from base type" OFF)
