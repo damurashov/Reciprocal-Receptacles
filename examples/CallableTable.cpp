e643546ea339403e4c3945699a1233c111196750
DmitryMurashov <dmtr.murashov@gmail.com>
2021-12-11 20:35:55 +0300

    [CallableTable] Impl

--- examples/CallableTable.cpp
+++ examples/CallableTable.cpp
@@ -5,6 +5,7 @@
 //  Author: Dmitry Murashov (dmtr <DOT> murashov <AT> <GMAIL>)
 //
 
+// #define RRO_STATIC_CAST_FN_CONVERSION 1
 #include <iostream>
 #include <Rr/Util/CallableTable.hpp>
 #include <list>
@@ -14,9 +15,6 @@ using namespace std;
 template <class Tsignature>
 struct A {
 };
-
-template <typename T1, typename ...T>
-using List = std::list<T1, T...>;
 
 void callMe(int, char)
 {
@@ -28,14 +26,25 @@ struct S {
 	{
 		cout << "A::callMe()" << std::endl;
 	}
+	void callMe(int, char) const
+	{
+		cout << "A::callMe() const" << std::endl;
+	}
 };
 
 void callableTable()
 {
 	S s;
-	Rr::Util::CallableTable<void(int, char), List> ct{{callMe}, {&S::callMe, &s}};
+	Rr::Util::CallableTable<void(int, char), std::list> ct{
+		{callMe},
+		{&S::callMe, &s}
+	};
+
 	ct.push_back(callMe);
 	ct.emplace_back(callMe);
+
+	ct.push_back({&S::callMe, (S *)&s});
+	ct.emplace_back((void (S::*)(int, char))&S::callMe, (S *)&s);
 
 	for (auto &callable : ct) {
 		callable(42, 'l');
@@ -44,9 +53,21 @@ void callableTable()
 
 void syncedCallableTable()
 {
-	Rr::Util::SyncedCallableTable<void(int, char) const, std::list, Rr::Trait::TsyncMut<1>> obst;
-	obst.emplace_back(callMe);
-	cout << obst.back().kGroup << endl;
+	const S s;
+	Rr::Util::GroupSyncedCallableTable<void(int, char) const, std::list, Rr::Trait::TsyncMut<1>> sct {
+		{callMe},
+		{&S::callMe, &s}
+	};
+
+	sct.push_back(callMe);
+	sct.emplace_back(callMe);
+
+	sct.push_back({&S::callMe, &s});
+	sct.emplace_back((void (S::*)(int, char)const)&S::callMe, &s);
+
+	for (auto &callable : sct) {
+		callable(42, 'l');
+	}
 }
 
 int main(void)
--- src/Rr/Util/CallableTable.hpp
+++ src/Rr/Util/CallableTable.hpp
@@ -10,49 +10,84 @@
 
 #include <Rr/Util/Callable.hpp>
 #include <Rr/Trait/Conditional.hpp>
+#include <Rr/Trait/EnableIf.hpp>
 #include <Rr/Trait/Sync.hpp>
 #include <Rr/Util/Sync.hpp>
 
 namespace Rr {
 namespace Util {
 
+namespace CallableTableImpl {
+
+///
+/// @brief Static storage for whatever sync primitive is being used
+///
+/// @tparam Tsync  See Trait/Sync
+/// @tparam Igroup If `Igroup != 0`, common (group) lock instance is used
+///
+template <class Tsync, unsigned Igroup>
+struct GroupSync {
+	static typename Tsync::Type value;
+};
+
+template <class Tsync, unsigned Igroup>
+typename Tsync::Type GroupSync<Tsync, Igroup>::value;
+
+///
+/// @brief Group lock variant
+///
+template <class Tsync, unsigned Igroup>
+struct GroupSyncedCallableBase {
+	typename Tsync::Type &mutex;
+	GroupSyncedCallableBase(): mutex(GroupSync<Tsync, Igroup>::value) {}
+};
+
+///
+/// @brief Individual lock variant
+///
+template <class Tsync>
+struct GroupSyncedCallableBase<Tsync, 0> {
+	typename Tsync::Type mutex;
+};
+
+}  // namespace CallableTableImpl
+
 template <class Tsignature, class Tsync>
-class SyncedCallable :
-	public Rr::Util::Callable<Tsignature>,
-	public Rr::Trait::InheritLockType<Tsync>
+class GroupSyncedCallable :
+	protected CallableTableImpl::GroupSyncedCallableBase<Tsync, Tsync::kGroup>,
+	public Rr::Util::Callable<Tsignature>
 {
-	bool enabled;
 public:
+	using CallableTableImpl::GroupSyncedCallableBase<Tsync, Tsync::kGroup>::GroupSyncedCallableBase;
 	using Rr::Util::Callable<Tsignature>::Callable;
 	using Rr::Util::Callable<Tsignature>::operator();
-	using Tsync::Type::Type;
 
-	// Helpers for Rr::Util::Lock;
-	using LockType = typename Rr::Trait::Conditional<Rr::Trait::Fn<Tsignature>::kConst, typename Tsync::ReadLockType, typename Tsync::WriteLockType>::Type;
-	using SyncType = typename Tsync::Type;
-	static constexpr auto kGroup = Tsync::kGroup;
-
-	template <class ...Ta>
-	SyncedCallable(Ta &&...aArgs) : Callable<Tsignature>(static_cast<Ta &&>(aArgs)...), enabled{true}
+	template <class ...TtArgs>
+	GroupSyncedCallable(bool aEnabled, TtArgs &&...aArgs):
+		CallableTableImpl::GroupSyncedCallableBase<Tsync, Tsync::kGroup>{},
+		Callable<Tsignature>(static_cast<TtArgs &&>(aArgs)...)
 	{
 	}
 
-	template <class ...Ta>
-	SyncedCallable(bool aEnabled, Ta &&...aArgs) : Callable<Tsignature>(static_cast<Ta &&>(aArgs)...), enabled{aEnabled}
+	template <typename ...TtArgs>
+	GroupSyncedCallable(TtArgs &&...aArgs): GroupSyncedCallable(true, static_cast<TtArgs &&>(aArgs)...)
 	{
-	}
-
-	void setEnabled(bool aEnabled)
-	{
-		enabled = aEnabled;
 	}
 };
 
 template <typename Tsignature, template <class ...> class Tcontainer>
 using CallableTable = Tcontainer<Rr::Util::Callable<Tsignature>>;
 
-template <class Tsignature, template<class...> class Tcontainer, class Tsync = Rr::Trait::TsyncMut<0>>
-using SyncedCallableTable = Tcontainer<Rr::Util::SyncedCallable<Tsignature, Tsync>>;
+template <class Tsignature, template<class...> class Tcontainer, class Tsync>
+using GroupSyncedCallableTable = Tcontainer<Rr::Util::GroupSyncedCallable<Tsignature, Tsync>>;
+
+///
+/// @brief Extension point, if other sync. mechanisms will be used
+///
+template <class Tsignature, template<class...> class Tcontainer, class Tsync>
+struct CallableTableType {
+	using Type = GroupSyncedCallableTable<Tsignature, Tcontainer, Tsync>;
+};
 
 }  // namespace Util
 }  // namespace Rr
