4adbc55d73c6066fd2437eb023f166c72e86ed31
DmitryMurashov <dmtr.murashov@gmail.com>
2021-12-13 09:51:20 +0300

    [trait: IntegralConstant] Impl

---
+++ src/Rr/Trait/IntegralConstant.hpp
@@ -0,0 +1,22 @@
+//
+// IntegralConstant.hpp
+//
+// Created: 2021-12-13
+//  Author: Dmitry Murashov (dmtr <DOT> murashov <AT> <GMAIL>)
+//
+
+#if !defined(RR_TRAIT_INTEGRALCONSTANT_HPP)
+#define RR_TRAIT_INTEGRALCONSTANT_HPP
+
+namespace Rr {
+namespace Trait {
+
+template<class T, T Vval>
+struct IntegralConstant {
+	static constexpr T value = Vval;
+};
+
+}  // namespace Trait
+}  // namespace Rr
+
+#endif // RR_TRAIT_INTEGRALCONSTANT_HPP
