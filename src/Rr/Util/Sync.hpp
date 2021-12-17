1a471c7a7e9f7c6d993049e0a5ae8c3f36c9e980
DmitryMurashov <dmtr.murashov@gmail.com>
2021-12-17 10:20:33 +0300

    [Sync] Ref | Dead code

--- src/Rr/Util/Sync.hpp
+++ src/Rr/Util/Sync.hpp
@@ -13,65 +13,6 @@
 namespace Rr {
 namespace Util {
 
-// GroupSync
-
-namespace GroupSyncImpl {
-
-///
-/// @brief Static storage for whatever sync primitive is being used
-///
-/// @tparam Tsync  See Trait/Sync
-/// @tparam Igroup If `Igroup != 0`, common (group) lock instance is used
-///
-template <class Tsync, unsigned Igroup>
-struct SyncInstance {
-	static typename Tsync::Type value;
-};
-
-template <class Tsync, unsigned Igroup>
-typename Tsync::Type SyncInstance<Tsync, Igroup>::value;
-
-}  // namespace GroupSyncImpl
-
-///
-/// @brief Group lock variant
-///
-template <class Tsync, unsigned Igroup>
-class GroupSync {
-	typename Tsync::Type &syncPrimitive;
-protected:
-	void deleteSyncPrimitive()
-	{
-	}
-public:
-	GroupSync(): syncPrimitive(GroupSyncImpl::SyncInstance<Tsync, Igroup>::value) {}
-	typename Tsync::Type &getSyncPrimitive()
-	{
-		return syncPrimitive;
-	}
-};
-
-///
-/// @brief Individual lock variant
-///
-template <class Tsync>
-class GroupSync<Tsync, 0> {
-	typename Tsync::Type *syncPrimitive;  // TODO: use shared pointer
-protected:
-	void deleteSyncPrimitive()
-	{
-		delete syncPrimitive;
-	}
-public:
-	GroupSync(): syncPrimitive{new typename Tsync::Type()}
-	{
-	}
-	typename Tsync::Type &getSyncPrimitive()
-	{
-		return *syncPrimitive;
-	}
-};
-
 template <class Tprimitive, class TholderType = Tprimitive *>
 class SyncPrimitiveHolder {
 	TholderType syncPrimitive;
