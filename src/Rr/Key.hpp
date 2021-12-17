28911be6879904fcafe731fd4792e821585c2658
DmitryMurashov <dmtr.murashov@gmail.com>
2021-12-17 10:13:39 +0300

    [Key] Enh Ref | Adopted the new compile-time lock policy hints, added comments

--- src/Rr/Key.hpp
+++ src/Rr/Key.hpp
@@ -12,28 +12,63 @@
 
 namespace Rr {
 
+///
+/// @brief  No-return notifications. Essentially, a callback table
+///
+/// @tparam Tsignature Signature of a method in form `void(int) [const]`
+/// @tparam Ttopic     Mock structure to provide a distinction
+/// @tparam Tstorage   Generic std::list-like container. std::list will fit perfectly
+/// @tparam Tsync      Synchronization traits. TODO: provide exhaustive description
+///
 template <class Tsignature, class Ttopic, template <class...> class Tstorage, class Tsync>
 class Key :
 	protected Rr::Util::Callable<Tsignature>,
 	protected Rr::Util::SyncedCallableWrapper<Tsignature, Tsync>
 {
 private:
+
+	///
+	/// @brief Puts the pointer to an instance to a callable table. The pointer
+	/// is stored statically, so constructing an instance does bear a little
+	/// overhead.
+	///
+	/// @param aInstance
+	///
 	static void registerTable(Key &aInstance)
 	{
 		WrapperTableType::asUniqueLockWrap().getInstance().push_back(aInstance);
 	}
 protected:
 	using WrapperTableType = typename Rr::Util::SyncedCallableWrapperStaticTable<Tsignature, Ttopic, Tstorage, Tsync>;
+	using Rr::Util::SyncedCallableWrapper<Tsignature, Tsync>::SetEnabledLockType;  ///< Different lock approaches are used for different actions
 
 public:
 	~Key() {
 		setEnabled(false);  // Safely disable the instance, so no attempts to invoke it will be made
 	}
 
+	///
+	/// @brief When disabled, the instance will stop receiving any
+	/// notifications. Automatically disabled on destruction; however, it gives
+	/// no guarantees, so it is better to disable it manually. Providing that the
+	/// Tsync trait is formed correctly, the method is thread-safe.
+	///
+	/// @param aEnabled
+	///
 	void setEnabled(bool aEnabled);
 
 	Key() = delete;
 
+	///
+	/// @brief Construct a new Key object
+	///
+	/// @tparam Ta
+	///
+	/// @param  aArgs (1) function pointer / (1) method pointer, (2) instance
+	/// pointer. NOTE: for overloaded methods (i.e. with reference /
+	/// cv-quailifiers) the method pointer should be explicitly casted to an
+	/// appropriate form.
+	///
 	template <class ...Ta>
 	Key(Ta &&...aArgs):
 		Key(true, static_cast<Ta &&>(aArgs)...)
