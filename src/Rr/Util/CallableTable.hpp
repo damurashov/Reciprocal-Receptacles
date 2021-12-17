d2fc6e2708ee80e65650ebc6d79a603d1247d08a
DmitryMurashov <dmtr.murashov@gmail.com>
2021-12-17 10:55:49 +0300

    [CallableTable] Impl | LockPolicy

--- src/Rr/Util/CallableTable.hpp
+++ src/Rr/Util/CallableTable.hpp
@@ -16,6 +16,37 @@ namespace Util {
 template <class Tsignature, template <class ...> class Tcontainer>
 using CallableTable = Tcontainer<Rr::Util::Callable<Tsignature>>;
 
+namespace CallableTableImpl {
+
+template <class Tsignature, class Tsync>
+class LockPolicy {
+private:
+	static constexpr auto kSyncTraitId = Rr::Trait::ToSyncTraitId<Tsync>::value;
+	static constexpr auto kNonSfinaeSyncTraitId = Rr::Trait::ToSyncTraitId<Tsync>::kNonSfinaeValue;
+	static constexpr auto kIsMutexBased = Rr::Trait::IntegralIn<Rr::Trait::SyncTraitId, /* operand */kSyncTraitId,
+		/* list */Rr::Trait::SyncTraitId::IndividualShared, Rr::Trait::SyncTraitId::IndividualUnique,
+		Rr::Trait::SyncTraitId::GroupUnique>::value;
+	static constexpr auto kIsMock = (kNonSfinaeSyncTraitId == Rr::Trait::SyncTraitId::NoSync);
+
+	static_assert(kIsMutexBased || kIsMock, "Non mutex-based sync types are not supported yet");
+
+	using MutType = typename Rr::Trait::AsMutTrait<Tsync>::Type;
+
+	template <class ...>
+	struct Tlist;
+
+	template <Rr::Trait::SyncTraitId ...>
+	struct Ilist;
+
+public:
+	using UniqueLockType = typename MutType::UniqueLock;
+	using SharedLockType = typename Rr::Trait::Conditional</* if */kNonSfinaeSyncTraitId ==
+		Rr::Trait::SyncTraitId::IndividualShared, typename MutType::SharedLock, /* else */ typename
+		MutType::UniqueLock>::Type;
+};
+
+}  // namespace CallableTableImpl
+
 template <class Tsignature, class Ttopic, template <class ...> class Tcontainer, class Tsync>
 class SyncedCallableWrapperStaticTable {
 
