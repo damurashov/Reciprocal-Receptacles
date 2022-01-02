9813c343c6f46237deda4c7cdbf49e6b9c524940
DmitryMurashov <dmtr.murashov@gmail.com>
2022-01-02 16:42:02 +0300

    [policy:Type] Impl

---
+++ src/Rr/Sync/Policy/Type.hpp
@@ -0,0 +1,19 @@
+//
+// Type.hpp
+//
+// Created on: 2022-01-02
+//     Author: Dmitry Murashov (dmtr <DOT> murashov <AT> <GMAIL>)
+//
+
+namespace Rr {
+namespace Sync {
+namespace Policy {
+
+enum class Type {
+    None,
+    Mutex,
+};
+
+}  // namespace Policy
+}  // namespace Sync
+}  // namespace Rr
