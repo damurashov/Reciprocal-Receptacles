14ddb114d584d3d357e8c5610909d215d5fd5906
DmitryMurashov <dmtr.murashov@gmail.com>
2022-01-09 17:58:42 +0300

    [policy:Type] Ref | Indent.

--- src/Rr/Sync/Policy/Type.hpp
+++ src/Rr/Sync/Policy/Type.hpp
@@ -5,15 +5,20 @@
 //     Author: Dmitry Murashov (dmtr <DOT> murashov <AT> <GMAIL>)
 //
 
+#if !defined(RR_SYNC_POLICY_TYPE_HPP)
+#define RR_SYNC_POLICY_TYPE_HPP
+
 namespace Rr {
 namespace Sync {
 namespace Policy {
 
 enum class Type {
-    None,
-    Mutex,
+	None,
+	Mutex,
 };
 
 }  // namespace Policy
 }  // namespace Sync
 }  // namespace Rr
+
+#endif // RR_SYNC_POLICY_TYPE_HPP
