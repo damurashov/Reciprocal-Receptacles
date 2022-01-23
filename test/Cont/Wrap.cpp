4c38f1959c728987b649ace1dc7533697f0f43ec
DmitryMurashov <dmtr.murashov@gmail.com>
2022-01-23 17:52:54 +0300

    [cont:Wrap] Impl | Test for wrap over Rr::Array

--- test/Cont/Wrap.cpp
+++ test/Cont/Wrap.cpp
@@ -8,20 +8,23 @@
 #include <cassert>
 #include <doctest/doctest.h>
 #include <Rr/Cont/Wrap.hpp>
+#include <Rr/Cont/Array.hpp>
 #include <array>
 
 template <class T>
 using Vec3d = std::array<T, 3>;
 
-struct Pod {
-	int a;
-	char b;
-};
+template <class T>
+using RrVec3d = typename Rr::Cont::Array<T, 3>;
 
 TEST_CASE("Cont::Wrap")
 {
 	SUBCASE("wrap std::array")
 	{
+		struct Pod {
+			int a;
+			char b;
+		};
 		Rr::Cont::Wrap<Pod, Vec3d> wrap;
 
 		auto it = wrap.begin();
@@ -46,7 +49,44 @@ TEST_CASE("Cont::Wrap")
 		CHECK(wrap.size() == 3);
 
 		for (auto &pod : wrap) {
-			(void) pod;
+			(void)pod;
+		}
+	}
+
+	SUBCASE("wrap Rr::Cont::Array")
+	{
+		struct NonPod {
+			int a;
+			char b;
+			NonPod(char aB): b(aB), a(aB)
+			{
+			}
+			NonPod(): a(42), b(42)
+			{
+			}
+		};
+
+		Rr::Cont::Wrap<NonPod, RrVec3d> wrap;
+
+		auto it = wrap.begin();
+		wrap.push({1});
+		CHECK(1 == it->a);
+
+		++it;
+		wrap.emplace();
+		CHECK(42 == it->a);
+		CHECK(2 == wrap.size());
+
+		++it;
+		wrap.push({3});
+		CHECK(3 == it->b);
+		CHECK(3 == wrap.size());
+
+		++it;
+		CHECK(it == wrap.end());
+
+		for (auto &nonpod : wrap) {
+			(void)nonpod;
 		}
 	}
 }
