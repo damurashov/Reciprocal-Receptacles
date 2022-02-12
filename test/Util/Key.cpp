0bf3467cfcd9b7287ac5526ebfc607844a9e0200
DmitryMurashov <dmtr.murashov@gmail.com>
2022-02-12 21:19:55 +0300

    [test:Key] Impl
    Tests for the newly-introduced key

---
+++ test/Util/Key.cpp
@@ -0,0 +1,87 @@
+//
+// Key.cpp
+//
+// Created on:  Feb 12, 2022
+//     Author: Dmitry Murashov (dmtr <AT> murashov <DOT> gmail.com)
+//
+
+#include <cassert>
+#include <doctest/doctest.h>
+#include <Rr/Util/Key.hpp>
+#include <mutex>
+#include <list>
+
+namespace TestKey {
+
+enum Ret {
+	Correct = 42,
+	Incorrect,
+};
+
+using Signature = Ret(int)const;
+
+struct SyncTrait {
+	static constexpr auto kPolicy = Rr::Sync::Policy::Type::Mutex;
+	using Mutex = std::mutex;
+};
+
+using Key = Rr::Util::Key<Signature, SyncTrait, std::list>;
+
+struct Call {
+	Ret doCall(int) const
+	{
+		++ncalled;
+		return Ret::Correct;
+	}
+
+	Ret doCall(int)
+	{
+		return Ret::Incorrect;
+	}
+
+	Key key;
+
+	Call(): key{&Call::doCall, this}
+	{
+	}
+
+	static int ncalled;
+
+	static void reset()
+	{
+		ncalled = 0;
+	}
+};
+
+int Call::ncalled = 0;
+
+}  // TestKey
+
+TEST_CASE("Key") {
+	TestKey::Call call;
+	TestKey::Call::reset();
+
+	SUBCASE("All enabled") {
+		for (auto &callable : TestKey::Key::getIterators()) {
+			CHECK(TestKey::Ret::Correct == callable(0xffff));
+		}
+
+		CHECK(1 == TestKey::Call::ncalled);
+	}
+
+	SUBCASE("Enabled / disabled") {
+		call.key.setEnabled(false);
+		for (auto &callable : TestKey::Key::getIterators()) {
+			CHECK(TestKey::Ret::Correct == callable(0));
+		}
+
+		CHECK(0 == TestKey::Call::ncalled);
+
+		call.key.setEnabled(true);
+		for (auto &callable : TestKey::Key::getIterators()) {
+			CHECK(TestKey::Ret::Correct == callable(0));
+		}
+
+		CHECK(1 == TestKey::Call::ncalled);
+	}
+}
