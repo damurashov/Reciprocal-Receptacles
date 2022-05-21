9b513b7df09f7ae605f413a44690be7809671f4a
DmitryMurashov <dmtr.murashov@gmail.com>
2022-05-21 14:21:13 +0300

    [Cont:Variant] Bug | Construction

--- src/Rr/Cont/Variant.hpp
+++ src/Rr/Cont/Variant.hpp
@@ -12,6 +12,7 @@
 #include <Rr/Trait/IntegralToType.hpp>
 #include <Rr/Trait/TypeToIntegral.hpp>
 #include <Rr/Trait/Forward.hpp>
+#include <Rr/Trait/RemoveReference.hpp>
 #include <Rr/Util/DefaultConfig.hpp>
 
 namespace Rr {
@@ -27,70 +28,42 @@ bool holdsAlternative(const Variant<Tvargs...> &aArgs);
 template <class ...Ta>
 class Variant final {
 private:
-	template <int I>
-	using GetType = typename Rr::Trait::IntToType<I, Ta...>;
+//	template <int I>
+//	using GetTypeTp = typename Rr::Trait::IntToType<I, Ta...>::Type;
 
 	template <class T>
 	using GetIndex = typename Rr::Trait::TypeToInt<T, Ta...>;
 
 public:
+	Variant() : index{-1}
+	{
+	}
+
 	template <class T>
-	Variant(T &&aValue) : index{GetIndex<T>::value}
+	Variant(T &&aValue) : index{-1}
 	{
 		static_assert(Rr::Trait::TypeIn<T, Ta...>::value, "The provided type is not among those the variant can hold");
-		emplace<T>(Rr::Trait::forward(aValue));
+		emplace<T>(Trait::forward<T>(aValue));
 	}
 
 	~Variant()
 	{
-		destruct<0>();
 	}
 
-
 	template <class T, class ...Ts>
-	void emplace(Ts &&...aArgs)
+	T &emplace(Ts &&...aArgs)
 	{
-		destruct<0>();
+		static_assert(Rr::Trait::TypeIn<T, Ta...>::value, "The provided type is not among those the variant can hold");
 		index = GetIndex<T>::value;
-		new (cell, Rr::Object()) T{Rr::Trait::forward<T>(aArgs)...};
+		new (this->cell, Rr::Object()) T{Rr::Trait::forward<T>(aArgs)...};
+
+		return *reinterpret_cast<T *>(cell);
 	}
 
 	template <class Talt, class ...Tvargs>
 	friend bool holdsAlternative(const Variant<Tvargs...> &);
 
 private:
-	template <int I, class T>
-	auto applyVisitor(T &&aVisitor) -> decltype(aVisitor(*reinterpret_cast<typename GetType<I>::Type*>(this->cell)))
-	{
-		if (index > 0) {
-			if (I == index) {
-				return aVisitor(*reinterpret_cast<typename GetType<I>::Type*>(cell));
-			} else {
-				return applyVisitor<I + 1>(Rr::Trait::forward(aVisitor));
-			}
-		}
-	}
-
-	template <class T>
-	auto applyVisitor(T &&visitor) -> decltype(applyVisitor<0>(Rr::Trait::forward(visitor)))
-	{
-		applyVisitor<0>(Rr::Trait::forward(visitor));
-	}
-
-	template <int I>
-	inline void destruct()
-	{
-		using Type = typename GetType<I>::Type;
-
-		if (index > 0 && I < sizeof...(Ta)) {
-			if (holdsAlternative<Type>(*this)) {
-				reinterpret_cast<Type *>(cell)->~Type();
-				index = -1;
-			} else {
-				destruct<I + 1>();
-			}
-		}
-	}
 
 private:
 	alignas(decltype(sizeof(int*))) unsigned char cell[Rr::Trait::MaxSizeof<Ta...>::value];
