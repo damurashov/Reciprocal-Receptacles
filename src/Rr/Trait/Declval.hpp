b49332ba2be02f7f5ed860d9800b55efd5dd5180
DmitryMurashov <dmtr.murashov@gmail.com>
2022-05-21 20:37:27 +0300

    [Trait:Declval] Impl | Idiomatic `declval`

--- src/Rr/Trait/Declval.hpp
+++ src/Rr/Trait/Declval.hpp
@@ -8,6 +8,9 @@
 #if !defined(RR_TRAIT_DECLVAL_HPP)
 #define RR_TRAIT_DECLVAL_HPP
 
+#include <Rr/Trait/AddCvpref.hpp>
+#include <Rr/Trait/IntegralConstant.hpp>
+
 namespace Rr {
 namespace Trait {
 namespace DeclvalImpl {
@@ -15,8 +18,20 @@ namespace DeclvalImpl {
 static constexpr int p1 = 0;
 static constexpr const void *p2 = &p1;
 
+template <class T>
+struct AlwaysFalse : FalseType {
+};
+
+template <class T>
+typename AddRvalueReference<T>::Type declval() noexcept
+{
+	static_assert(AlwaysFalse<T>::value, "Use of declval in an evaluated context");
+}
+
 }  // namespace DeclvalImpl
 
+/// \brief Non-idiomatic implementation of STL-like declval that can be used in an evaluated context
+///
 template <class T>
 constexpr T &eDeclval()
 {
