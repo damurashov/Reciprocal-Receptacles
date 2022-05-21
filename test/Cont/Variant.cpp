aa2c7dd67b822541d1a80265102f0a0e584a1a1b
DmitryMurashov <dmtr.murashov@gmail.com>
2022-05-21 09:54:06 +0300

    [Cont:Variant] Impl | Initial

---
+++ test/Cont/Variant.cpp
@@ -0,0 +1,20 @@
+//
+// Wrap.cpp
+//
+// Created on: 2022-01-22
+//     Author: Dmitry Murashov (dmtr <DOT> murashov <AT> <GMAIL>)
+//
+
+#include <doctest/doctest.h>
+#include <Rr/Cont/Variant.hpp>
+
+TEST_CASE("Cont::Variant")
+{
+	SUBCASE("dummy")
+	{
+	}
+
+	SUBCASE("dummy2")
+	{
+	}
+}
