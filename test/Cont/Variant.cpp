14ae1d9e72308a627021a881ddd5eb09fb919f9c
DmitryMurashov <dmtr.murashov@gmail.com>
2022-05-21 20:24:39 +0300

    [test:Variant] Impl

--- test/Cont/Variant.cpp
+++ test/Cont/Variant.cpp
@@ -1,20 +1,32 @@
 //
-// Wrap.cpp
+// Variant.cpp
 //
-// Created on: 2022-01-22
-//     Author: Dmitry Murashov (dmtr <DOT> murashov <AT> <GMAIL>)
+// Created on:  May 21, 2022
+//     Author: Dmitry Murashov (dmtr <AT> murashov <DOT> gmail.com)
 //
 
 #include <doctest/doctest.h>
 #include <Rr/Cont/Variant.hpp>
+#include <string>
 
-TEST_CASE("Cont::Variant")
+TEST_CASE("Cont::Variant, construction")
 {
-	SUBCASE("dummy")
-	{
+	using Variant = Rr::Cont::Variant<int, std::string, char>;
+
+	SUBCASE("move construction") {
+		Variant variant1{std::string("")};
+		Variant variant2{0};
+		Variant variant3{'c'};
+
+		CHECK(Rr::Cont::Vrnt::holdsAlternative<std::string>(variant1));
+		CHECK(Rr::Cont::Vrnt::holdsAlternative<char>(variant3));
+		CHECK(Rr::Cont::Vrnt::holdsAlternative<int>(variant2));
 	}
 
-	SUBCASE("dummy2")
-	{
+	SUBCASE("copy construction") {
+		std::string valString;
+
+		Variant variant1{valString};
+		CHECK(Rr::Cont::Vrnt::holdsAlternative<std::string>(variant1));
 	}
 }
