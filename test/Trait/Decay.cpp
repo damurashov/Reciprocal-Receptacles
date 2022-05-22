c12844a570ef243819bbb4ed8f6b8273996747c6
DmitryMurashov <dmtr.murashov@gmail.com>
2022-05-22 14:03:28 +0300

    [test:Decay] Impl

---
+++ test/Trait/Decay.cpp
@@ -0,0 +1,31 @@
+//
+// Decay.cpp
+//
+// Created on:  May 22, 2022
+//     Author: Dmitry Murashov (dmtr <AT> murashov <DOT> gmail.com)
+//
+
+#include <Rr/Trait/Decay.hpp>
+#include <Rr/Trait/IsSame.hpp>
+#include <doctest/doctest.h>
+
+using namespace Rr::Trait;
+
+template <class Tdecay, class Tref>
+static constexpr bool checkDecayEq()
+{
+	return Rr::Trait::IsSame<Tref, typename Rr::Trait::Decay<Tdecay>::Type>::value;
+}
+
+TEST_CASE("Trait::Decay") {
+	CHECK(checkDecayEq<int, int>());
+	CHECK(checkDecayEq<int[], int *>());
+	CHECK(checkDecayEq<int[4], int *>());
+	CHECK(checkDecayEq<int &, int>());
+	CHECK(checkDecayEq<int &&, int>());
+	CHECK(checkDecayEq<const int &, int>());
+	CHECK(checkDecayEq<const int *, const int *>());
+	CHECK(checkDecayEq<const int * const , const int *>());
+	CHECK(checkDecayEq<const int * volatile , const int *>());
+	CHECK(checkDecayEq<const int * const volatile , const int *>());
+}
