1e2900c4b9c01d0a4854e2f3e7397f191f534346
DmitryMurashov <dmtr.murashov@gmail.com>
2022-05-22 12:35:28 +0300

    [Trait:IsFunction] Impl

---
+++ src/Rr/Trait/IsFunction.hpp
@@ -0,0 +1,42 @@
+//
+// IsFunction.hpp
+//
+// Created on:  May 22, 2022
+//     Author: Dmitry Murashov (dmtr <AT> murashov <DOT> gmail.com)
+//
+
+#if !defined(RR_TRAIT_ISFUNCTION_HPP_)
+#define RR_TRAIT_ISFUNCTION_HPP_
+
+#include <Rr/Trait/IntegralConstant.hpp>
+#include <Rr/Trait/MemberDecay.hpp>
+#include <Rr/Trait/EnableIf.hpp>
+#include <Rr/Refl/CallFamily.hpp>
+
+namespace Rr {
+namespace Trait {
+namespace IsFunctionImpl {
+
+struct MethodSfinae {
+	template <class T, bool F = Trait::MemberDecay<T>::kIsMethod>
+	static typename Trait::EnableIf<F>::Type call();
+};
+
+template <class T>
+constexpr bool checkIsFunction()
+{
+	return Refl::CanCallFamily<MethodSfinae>::template check<T>();
+}
+
+template <class T>
+struct IsFunction : Rr::Trait::IntegralConstant<bool, checkIsFunction<T>()> {
+};
+
+}  // IsFunctionImpl
+
+using IsFunctionImpl::IsFunction;
+
+}  // Trait
+}  // Rr
+
+#endif  // RR_TRAIT_ISFUNCTION_HPP_
