1e0627a3459a9a53c8406846f9ad9bcfef68422b
DmitryMurashov <dmtr.murashov@gmail.com>
2023-07-27 12:04:28 +0300

    [Port:Thread] Impl |

---
+++ src/Rr/Port/Thread.hpp
@@ -0,0 +1,47 @@
+//
+// Thread.hpp
+//
+// Created on: Jul 27, 2023
+//     Author: Dmitry Murashov (dmtr <DOT> murashov <AT> <GMAIL> <DOT> <COM>)
+//
+
+#ifndef RR_PORT_THREAD_HPP
+#define RR_PORT_THREAD_HPP
+
+namespace Rr {
+namespace Port {
+
+/// Callable for putting a current thread into delay for a minimum amount of
+/// time.
+using ThreadSleepCallable = void(*)();
+
+namespace ThreadApiImpl {
+
+template <class T>
+class ThreadApi {
+public:
+	void setThreadSleepCallable(ThreadSleepCallable);
+
+	/// Invokes sleep (yield control) function, if set
+	void trySleep()
+	{
+		if (threadSleepCallable != nullptr) {
+			threadSleepCallable();
+		}
+	}
+
+private:
+	static ThreadSleepCallable threadSleepCallable;
+};
+
+template <class T>
+ThreadSleepCallable ThreadApi<T>::threadSleepCallable{nullptr};
+
+}  // namespace ThreadApiImpl
+
+using Thread = typename ThreadApiImpl::ThreadApi<void>;
+
+}  // Port
+}  // Rr
+
+#endif // RR_PORT_THREAD_HPP
